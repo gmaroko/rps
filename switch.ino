2b7229fd40beda26684744fe6188c119130098d3
gmaroko <marokogideon@gmail.com>
2017-12-04 03:12:49 +0300

    c_view.PNG

---
+++ switch.ino
@@ -0,0 +1,35 @@
+const int sPin = 2;
+const int camPin = 4;
+int sStatus = 0;
+
+void setup(){
+  pinMode(sPin, INPUT); //listen at dpin 2
+  Serial.begin(9600);
+  
+}
+void loop(){
+  sStatus = digitalRead(sPin);
+  //digitalWrite(vPin, HIGH);
+  if (takenPic(sStatus)){
+    Serial.println("Switch is on, taking pic...!");
+    }
+    else{
+      Serial.println("Switch is off");
+      }
+
+  }
+
+
+bool takenPic(int statusP){ 
+  if(statusP == HIGH){
+    digitalWrite(camPin, HIGH);
+    return camPin == LOW;
+    }
+    else
+    {
+    digitalWrite(camPin, LOW);
+    return camPin == HIGH;
+    }
+
+  
+  }
